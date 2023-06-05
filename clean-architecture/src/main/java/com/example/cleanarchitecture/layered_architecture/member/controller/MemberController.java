3fb15b5f0571b615233718d0295cdd3868fbd672
yhnoh <tedgom19@gmail.com>
2023-06-05 10:30:06 +0900

    인커밍 어댑터 알아보기

--- clean-architecture/src/main/java/com/example/cleanarchitecture/layered_architecture/member/controller/MemberController.java
+++ clean-architecture/src/main/java/com/example/cleanarchitecture/layered_architecture/member/controller/MemberController.java
@@ -2,11 +2,16 @@ package com.example.cleanarchitecture.layered_architecture.member.controller;
 
 import com.example.cleanarchitecture.layered_architecture.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class MemberController {
     private final MemberService memberService;
+
+    public void members(@RequestParam("")){
+
+    }
 
 }
