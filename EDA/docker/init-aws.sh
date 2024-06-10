fdc44460ea90bc6d3e3ca9c35e6aba69952d55a9
yhnoh <tedgom19@gmail.com>
2024-06-11 00:43:36 +0900

    이벤트 기반 아키텍처 구축 여정기

--- EDA/docker/init-aws.sh
+++ EDA/docker/init-aws.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
+
 awslocal sqs create-queue --queue-name my-queue
+
+awslocal sns create-topic --name my-topic
+
+awslocal sns subscribe \
+    --topic-arn "arn:aws:sns:us-east-1:000000000000:my-topic" \
+    --protocol sqs \
+    --notification-endpoint "arn:aws:sqs:us-east-1:000000000000:my-queue"
+
+
+
+
+## awslocal sns publish --topic-arn "arn:aws:sns:us-east-1:000000000000:my-topic" --message "hello"
+## awslocal sqs receive-message --queue-url "http://sqs.us-east-1.localhost.localstack.cloud:4566/000000000000/my-queue"
