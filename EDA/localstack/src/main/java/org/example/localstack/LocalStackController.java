9ed76de405bae19124ee2c5989be27ec68510702
yhnoh <tedgom19@gmail.com>
2024-06-18 20:53:54 +0900

    Spring Application 과 LocalStack 연동하기

---
+++ EDA/localstack/src/main/java/org/example/localstack/LocalStackController.java
@@ -0,0 +1,38 @@
+package org.example.localstack;
+
+import io.awspring.cloud.sns.core.SnsTemplate;
+import io.awspring.cloud.sqs.annotation.SqsListener;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class LocalStackController {
+
+    private final SnsTemplate snsTemplate;
+
+    @GetMapping("hello-world")
+    public void helloWorld() {
+
+        MessageBody messageBody = new MessageBody();
+        messageBody.setMessage("helloWorld");
+        snsTemplate.convertAndSend("my-topic", messageBody);
+    }
+
+    @Getter
+    @Setter
+    @ToString
+    public static class MessageBody {
+        private String message = "hello world";
+    }
+
+    @SqsListener("my-queue")
+    public void receiveMessage(@Payload MessageBody messageBody) {
+        System.out.println("messageBody = " + messageBody);
+    }
+}
